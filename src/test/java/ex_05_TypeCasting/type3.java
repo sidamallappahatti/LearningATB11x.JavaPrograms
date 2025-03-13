86046ab780367455d4ba964042ffaae057b7c76c
Sidduhatti <sdhatti007@gmail.com>
2025-03-13 12:13:44 +0530

    March 13th lab Exercises

--- src/test/java/ex_05_TypeCasting/type3.java
+++ src/test/java/ex_05_TypeCasting/type3.java
@@ -6,12 +6,9 @@ public class type3 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter Score Value: ");
-        int Score = scanner.nextInt();
+        System.out.println("Enter Score: ");
 
-        String grade = (Score >= 90) ? "A" : (Score >= 80) ? "B" : (Score >= 70) ? "C" : (Score >= 60) ? "D" : "F";
 
-        System.out.println(grade);
         }
     }
 
