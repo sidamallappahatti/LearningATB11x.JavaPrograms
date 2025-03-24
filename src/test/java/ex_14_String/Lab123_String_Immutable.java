9eac9e529b4bdad0bde70a062637ca20946bc78b
Sidduhatti <sdhatti007@gmail.com>
2025-03-24 10:13:01 +0530

    add code

--- src/test/java/ex_14_String/Lab123_String_Immutable.java
+++ src/test/java/ex_14_String/Lab123_String_Immutable.java
@@ -1,4 +1,10 @@
 package ex_14_String;
 
 public class Lab123_String_Immutable {
+    public static void main(String[] args) {
+        String name = "siddu";
+        name.toUpperCase(); //
+        System.out.println(name);
+
     }
+}
