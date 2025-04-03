6cb20f4a60de3dd2371cc73fe2cc25d5617f4bf7
Sidduhatti <sdhatti007@gmail.com>
2025-04-03 20:43:28 +0530

    Lab exercise 31st March

---
+++ src/test/java/ex_17_OOPs/ATB.java
@@ -0,0 +1,29 @@
+package ex_17_OOPs;
+
+public class ATB {
+
+    // Attributes
+    String name;
+    int age;
+    String course;
+    String email;
+    String gender;
+
+
+    //Behaviour
+    void speak() {
+    }
+
+    void listen() {
+    }
+
+    void doTask() {
+    }
+
+    void askQuestion() {
+    }
+
+    void learnSomething() {
+    }
+
+}
---
+++ src/test/java/ex_17_OOPs/Lab_157.java
@@ -0,0 +1,20 @@
+package ex_17_OOPs;
+
+public class Lab_157 {
+    public static void main(String[] args) {
+        System.out.println("157");
+        System.out.println("One main is enough");
+    }
+}
+
+class Lab_158 {
+    public static void main(String[] args) {
+        System.out.println("158");
+    }
+}
+
+class Lab_159 {
+    public static void main(String[] args) {
+        System.out.println("159");
+    }
+}
