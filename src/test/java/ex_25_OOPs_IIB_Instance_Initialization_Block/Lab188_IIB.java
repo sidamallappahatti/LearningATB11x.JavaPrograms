6daa2a0ec4d54f2ae971d5bee2a379d26bfdc566
sidamallappad <sdhatti007@gmail.com>
2025-06-09 21:25:01 +0530

    Lab exercise 8th April

---
+++ src/test/java/ex_25_OOPs_IIB_Instance_Initialization_Block/Lab188_IIB.java
@@ -0,0 +1,34 @@
+package ex_25_OOPs_IIB_Instance_Initialization_Block;
+
+public class Lab188_IIB {
+
+    //IIB ==> Instance initialization Block
+    //I will be executed when Object is created
+
+    public static void main(String[] args) {
+        new A1();
+    }
+}
+
+class A1{
+    A1(){
+        System.out.println("Default Constructor");
+    }
+
+    {
+        System.out.println("Hi, I am first IIB");
+        System.out.println("If you want to execute or call something when object is created");
+        // mysql connection
+        // read a csv, xlsx file
+        // read json, xml.
+        // read a text file or env file
+    }
+    {
+        System.out.println("Hi, I am Second IIB");
+    }
+
+    {
+        System.out.println("Hi, I am Third IIB");
+    }
+
+}
